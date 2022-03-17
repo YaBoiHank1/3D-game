4a938d2e9303fdcb13b9a950715b282cb826c58d
Henry Owen <hnryowen1@gmail.com>
2022-03-17 10:26:56 -0700

    Quest Text

--- My project/Assets/Scripts/ObjectiveTracker.cs
+++ My project/Assets/Scripts/ObjectiveTracker.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveTracker : MonoBehaviour
 {
     public int goal = 5;
+    public Text objectiveText;
     private int collected = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectiveText.text = collected.ToString() + "/" + goal.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        objectiveText.text = collected.ToString() + "/" + goal.ToString();
     }
 
     public void ObjectiveCollected()
