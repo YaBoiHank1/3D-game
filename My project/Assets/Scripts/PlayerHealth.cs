84ef65aa42aac8c79139b790498603011baa3bf9
Henry Owen <hnryowen1@gmail.com>
2022-03-28 08:53:42 -0700

    Game Over Canvas

--- My project/Assets/Scripts/PlayerHealth.cs
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public Collider myCollider;
+    public Canvas gameOverCanvas;
+    public Animator canvasAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
+        gameOverCanvas.enabled = false;
+        canvasAnimator = FindObjectOfType<Canvas>(gameOverCanvas).GetComponent<Animator>();
         myCollider = GetComponent<CapsuleCollider>();
         GetComponent<RigidbodyFirstPersonController>().enabled = true;
     }
@@ -26,5 +32,14 @@ public class PlayerHealth : MonoBehaviour
     {
         GetComponent<RigidbodyFirstPersonController>().enabled = false;
         transform.LookAt(FindObjectOfType <EnemyNavigation>().transform);
+        StartCoroutine(LoadGameOver());
+    }
+
+    private IEnumerator LoadGameOver()
+    {
+        yield return new WaitForSeconds(3);
+        
+        gameOverCanvas.enabled = true;
+        canvasAnimator.SetBool("FadeIn", true);
     }
 }
